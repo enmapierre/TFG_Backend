691859a73b06f4b9d59f2857861c9e322c4b7c42
enmapierre <enmapiedrafernandez@gmail.com>
2025-06-07 18:50:24 +0200

    Actualizacion del Back

--- servidor.js
+++ servidor.js
@@ -1,4 +1,4 @@
-
+const cors = require("cors")
 const express = require ("express")
 
 const Producto = require ("./models/producto.model")
@@ -10,6 +10,7 @@ const producto = require("./models/producto.model")
 mongoose.connect("mongodb+srv://enmapiedrafernandez:ht9mH1W9T69DzjpH@cluster0.iggszwx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 .then(()=>console.log("Conectado bbdd"))
 
+app.use(cors())
 app.use(express.json())
 app.listen(8080, ()=> console.log ('Servidor levantado'))
 
@@ -37,20 +38,44 @@ app.get("/productos/summer",(req,res) => { //end-point
     Producto.find({category:"summer"}).then(productos=>res.json(productos))
 })
 
-app.put("/productos/:id",async(req,res) => {
-    const {id}=req.params
-
-    const UpDateProduct = req.body
-    try{
-        const UpDatedProducto = await Producto.findByIdAndUpdate(id,UpDateProduct,{new : true})
-        if (!UpDatedProducto){
-            return res.status(404).json({message: "Error proucto no encontrado"})
+app.put("/productos/:id", async (req, res) => {
+    const { id } = req.params;
+    const updateProduct = req.body;
+    console.log("Datos recibidos:", updateProduct);
+    
+    try {
+        const oldProducto = await Producto.findById(id);
+        
+        if (!oldProducto) {
+            return res.status(404).json({ message: "Error: producto no encontrado" });
         }
-        res.json(UpDatedProducto)
-    }catch(err){
-        console.error(err)
-        res.status(500).json({message:"Error al actualizar producto"})
+        
+        console.log("Producto encontrado:", oldProducto);
+        console.log("Stock actual:", oldProducto.stock);
+        console.log("Stock a descontar:", updateProduct.stock);
+        
+        const nuevoStock = oldProducto.stock - parseInt(updateProduct.stock);
+        console.log("Nuevo stock:", nuevoStock);
+        
+        if (nuevoStock <= 0) {
+            return res.status(400).json({ 
+                message: "Stock insuficiente", 
+                stockDisponible: oldProducto.stock 
+            });
+        }
+        
+        const updatedProducto = await Producto.findByIdAndUpdate(
+            id,
+            { stock: nuevoStock },
+            { new: true } 
+        );
+        
+        console.log("Producto actualizado:", updatedProducto);
+        res.json(updatedProducto);
+        
+    } catch (err) {
+        console.error("Error completo:", err);
+        res.status(500).json({ message: "Error al actualizar producto" });
     }
-})
-
+});
 
